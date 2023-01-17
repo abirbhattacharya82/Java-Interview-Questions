a1431f2aa0c7614b4dc861f745e05dd140e19dd8
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2023-01-17 20:52:33 +0530

    Q6

---
+++ DelloiteNLA/Q7/Solution.java
@@ -0,0 +1,20 @@
+/*
+I was looking for sentence that contain all english alphabets character at least once in them the sentence help me building my NLP program to check how the user pronounce each and every character.
+I have got million of sentence can you help me with a program that accept n sentence and print 1 if they all are compatible with NLP system else print 0.
+*/
+import java.util.*;
+public class Solution
+{
+    public static void main(String argsp[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=Integer.parseInt(sc.nextLine());
+        char ch[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        for(int i=0;i<n;i++)
+        {
+            String a=sc.nextLine();
+            HashMap<Character,Integer>=new HashMap<>();
+            
+        }
+    }
+}
